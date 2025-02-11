e95266c4a783a3ddd17da2b180e39eea1c795a74
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-10 21:43:08 -0300

    Creating database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { DATABASE_CONECTION } from './database-connection';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, {
+          schema: {},
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
 export class DatabaseModule {}
