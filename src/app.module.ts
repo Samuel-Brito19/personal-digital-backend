0d6093160c252c5da9264229ae21009877348b86
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-11 20:46:04 -0300

    generating exercise modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PersonalModule } from './personal/personal.module';
+import { ExerciseModule } from './exercise/exercise.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule, ExerciseModule],
   controllers: [],
 })
 export class AppModule {}
