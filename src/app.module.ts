de341d30bb5c12764c0ab1033c9f6cdfb0e0483a
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-11 09:34:28 -0300

    Personal table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { PersonalModule } from './personal/personal.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule],
   controllers: [],
 })
 export class AppModule {}
