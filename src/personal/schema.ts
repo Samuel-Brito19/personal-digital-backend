69765cf73fa80a5717c6f9443fd202d96411da92
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-11 10:29:53 -0300

    personal and user relations

---
+++ src/personal/schema.ts
@@ -0,0 +1,15 @@
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import { users } from 'src/users/schema';
+
+export const personal = pgTable('personal', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text('description').notNull(),
+  instagram_link: text('instagram_link').notNull(),
+  whatsapp_link: text('whatsapp_link').notNull(),
+});
+
+export const personalRelations = relations(personal, ({ many }) => ({
+  client: many(users),
+}));
--- src/users/schema.ts
+++ src/users/schema.ts
@@ -1,8 +1,18 @@
+import { relations } from 'drizzle-orm';
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { personal } from 'src/personal/schema';
 
-export const usersTable = pgTable('users', {
+export const users = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  personal_id: integer('personal_id').references(() => personal.id),
 });
+
+export const usersRelation = relations(users, ({ one }) => ({
+  personal: one(personal, {
+    fields: [users.personal_id],
+    references: [personal.id],
+  }),
+}));
