944083e9d6ce0169fe74315fad910b873bc4f060
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-11 21:24:30 -0300

    generating groups modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PersonalModule } from './personal/personal.module';
 import { ExerciseModule } from './exercise/exercise.module';
 import { WorkoutsModule } from './workouts/workouts.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule, ExerciseModule, WorkoutsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule, ExerciseModule, WorkoutsModule, GroupsModule],
   controllers: [],
 })
 export class AppModule {}
