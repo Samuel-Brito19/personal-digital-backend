d65e655be1cd5a1a09a33bd46354079a2bc4cdcf
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-19 19:38:51 -0300

    creating user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,13 @@ import { ClientModule } from './client/client.module';
 import { ExerciseModuleModule } from './exercise-module/exercise-module.module';
 import { ExerciseWorkoutModule } from './exercise-workout/exercise-workout.module';
 import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule, ExerciseModule, WorkoutsModule, GroupsModule, ModelsModule, ClientModule, ExerciseModuleModule, ExerciseWorkoutModule, AuthModule],
-  controllers: [ModelsController],
-  providers: [ModelsService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, PersonalModule, ExerciseModule, WorkoutsModule, GroupsModule, ModelsModule, ClientModule, ExerciseModuleModule, ExerciseWorkoutModule, AuthModule, UserModule],
+  controllers: [ModelsController, UserController],
+  providers: [ModelsService, UserService],
 })
 export class AppModule {}
--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ClientController],
-  providers: [ClientService]
+  providers: [ClientService],
+  exports: [ClientService],
 })
 export class ClientModule {}
