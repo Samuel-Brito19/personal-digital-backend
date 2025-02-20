11f261c3bdaf199cc1f942a360f97adc1231a7fd
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-20 10:44:58 -0300

    auth guard service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.jwtConstants,
+      signOptions: { expiresIn: '3d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '56d89ba761adec2c26e2708b6e56fb2386cf70b013c46be4641b6ea1ff345a49',
+};
