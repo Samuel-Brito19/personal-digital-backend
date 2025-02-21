767aad27681bc8487f2c82f44a209b540d6225fd
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-21 11:10:19 -0300

    enabling authentication globally

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/create-user-dto';
+import { Public } from 'src/auth/decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Get()
   async findAllUsers() {
     return this.userService.getAllUsers();
   }
-
+  @Public()
   @Get(':name')
   async findUserByName(@Param('name') name: string) {
     return this.userService.findUser(name);
   }
-
+  @Public()
   @Post()
   async createUser(@Body() request: CreateUserDTO) {
     return this.userService.createUser(request);
