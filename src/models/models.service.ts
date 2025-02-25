e2982c2dcfa7bd239bbc408af19b9826bd5f008e
Samuel-Brito19 <sambriton7@gmail.com>
2025-02-25 17:42:21 -0300

    models service

--- src/models/models.service.ts
+++ src/models/models.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DATABASE_CONECTION } from 'src/database/database-connection';
+import * as schema from './schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class ModelsService {}
+export class ModelsService {
+  constructor(
+    @Inject(DATABASE_CONECTION)
+    private readonly database: NodePgDatabase<typeof schema>,
+  ) {}
+
+  async getModels(personalId: number) {
+    return this.database.query.models.findMany({
+      where: eq(schema.models.id, personalId),
+    });
+  }
+
+  async createModel(model: typeof schema.models.$inferInsert) {
+    return this.database.insert(schema.models).values(model).returning();
+  }
+
+  async updateModel(modelId, data: typeof schema.models.$inferInsert) {
+    return this.database
+      .update(schema.models)
+      .set(data)
+      .where(eq(schema.models.id, modelId))
+      .returning();
+  }
+}
